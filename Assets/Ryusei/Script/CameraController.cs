c8055eaa8ab88460ae3b8812dac9aaa1b2f7fe59
nishishimamoto <11807020@stu.kbc.ac.jp>
2020-10-14 23:33:22 +0900

    プレイヤーの移動方向に正面を向く

--- Assets/Ryusei/Script/CameraController.cs
+++ Assets/Ryusei/Script/CameraController.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    GameObject mainCamera;  //カメラ
-    //GameObject fieldObject; //フィールド
-    [SerializeField] GameObject Player;
-    public float rotateSpeed = 3.0f; //回転スピード
+    [SerializeField] private float turnSpeed = 6.0f;   // 回転速度
+    [SerializeField] private Transform player;          // 注視対象プレイヤー
+
+    [SerializeField] private float distance = 6.0f;    // 注視対象プレイヤーからカメラを離す距離
+    [SerializeField] private Quaternion vRotation;      // カメラの垂直回転(見下ろし回転)
+    [SerializeField] public Quaternion hRotation;      // カメラの水平回転
 
     void Start()
     {
-        this.mainCamera = Camera.main.gameObject; //カメラオブジェクトを代入
-        //this.fieldObject = GameObject.Find("Player");
-        //this.fieldObject = GameObject.Find("Floor1");
+        // カーソルを画面中央にロックする
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // 回転の初期化
+        //vRotation = Quaternion.Euler(25, 0, 0);         // 25度固定の垂直回転
+        vRotation = Quaternion.identity;         // 垂直回転(X軸を軸とする回転)
+        hRotation = Quaternion.identity;                // 水平回転(Y軸を軸とする回転)
+        transform.rotation = hRotation * vRotation;     // 最終的なカメラの回転は、垂直回転してから水平回転する合成回転
+
+        // 位置の初期化
+        transform.position = player.position - transform.rotation * Vector3.forward * distance;
     }
-    void Update()
+
+    void LateUpdate()
     {
-            rotateCamera();
-    }
-    private void rotateCamera()
-    {
-        Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * this.rotateSpeed, Input.GetAxis("Mouse Y") * this.rotateSpeed, 0);
-        this.mainCamera.transform.RotateAround(Player.transform.position, Vector3.up, angle.x);
-        this.mainCamera.transform.RotateAround(Player.transform.position, transform.right, angle.y);
+        // 水平回転の更新
+        //if (Input.GetMouseButton(0))
+        hRotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * turnSpeed, 0);  //垂直回転
+        vRotation *= Quaternion.Euler(Input.GetAxis("Mouse Y") * turnSpeed, 0, 0);  //水平回転
+
+        // カメラの回転(transform.rotation)の更新
+        transform.rotation = hRotation * vRotation;
+
+        // player位置から距離distanceだけ手前に引いた位置を設定
+        transform.position = player.position + new Vector3(0, 2, 0) - transform.rotation * Vector3.forward * distance;
     }
 }
--- Assets/Ryusei/Script/Player.cs
+++ Assets/Ryusei/Script/Player.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //public float speed = 1.0f;
+    [SerializeField] private Vector3 velocity;              // 移動方向
+    [SerializeField] private float moveSpeed = 5.0f;        // 移動速度
+    [SerializeField] private float applySpeed = 0.2f;       // 振り向きの適用速度
+    [SerializeField] private CameraController refCamera;  // カメラの水平回転を参照する用
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        // WASD入力から、XZ平面(水平な地面)を移動する方向(velocity)を得る
+        velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            velocity.z += 1;
+        if (Input.GetKey(KeyCode.A))
+            velocity.x -= 1;
+        if (Input.GetKey(KeyCode.S))
+            velocity.z -= 1;
+        if (Input.GetKey(KeyCode.D))
+            velocity.x += 1;
 
-    }
+        // 速度ベクトルの長さを1秒でmoveSpeedだけ進むように調整
+        velocity = velocity.normalized * moveSpeed * Time.deltaTime;
 
-    private void FixedUpdate()
-    {
-        var cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-        Vector3 direction = cameraForward * Input.GetAxis("Vertical") +
-                Camera.main.transform.right * Input.GetAxis("Horizontal");
+        // いずれかの方向に移動している場合
+        if (velocity.magnitude > 0)
+        {
+            // プレイヤーの回転(transform.rotation)の更新
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(refCamera.hRotation * velocity),
+                                                  applySpeed);
 
-        transform.position += direction / 20;
+            // プレイヤーの位置(transform.position)の更新
+            transform.position += refCamera.hRotation * velocity;
+        }
     }
 }
