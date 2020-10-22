e0fe3f69f5365dda99311f335dc12f4ba15d7c46
nishishimamoto <11807020@stu.kbc.ac.jp>
2020-10-22 15:58:54 +0900

    多分動くMapChip

---
+++ Assets/Ryusei/MapChipScript/R_ElectricalWire2.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class R_ElectricalWire2 : MonoBehaviour
+{
+
+    bool EnergizedFlg;
+    BoxCollider collider;
+    GameObject PowerButton;
+
+    int i = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+        PowerButton = GameObject.FindGameObjectWithTag("PowerButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        //i++;
+        //Debug.Log(i);
+        //if (other.gameObject.tag == "EnergizedOn" && PowerButton.tag == "EnergizedOn")
+        //{
+        //    this.tag = "EnergizedOn";
+        //    GetComponent<Renderer>().material.color = Color.yellow;
+        //}
+        //else if (other.gameObject.tag == "EnergizedOff" && PowerButton.tag == "EnergizedOff")
+        //{
+        //    this.tag = "EnergizedOff";
+        //    GetComponent<Renderer>().material.color = Color.black;
+        //}
+
+        //collider.size = new Vector3(0, 0, 0); //
+        //Invoke("DelayMethod", 0.02f);
+        if (other.gameObject.tag != "Player" && other.gameObject.tag != "ElectricalDoor")
+        {
+            this.tag = "BlackOut";
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == "BlackOut")
+        {
+            this.tag = "BlackOut";
+        }
+
+        if (gameObject.tag == "BlackOut")
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+            Invoke("DelayMethod", 0.02f);
+        }
+        else if (other.gameObject.tag == "EnergizedOn" && PowerButton.tag == "EnergizedOn")
+        {
+            this.tag = "EnergizedOn";
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else if (other.gameObject.tag == "EnergizedOff" && PowerButton.tag == "EnergizedOff")
+        {
+            this.tag = "EnergizedOff";
+            GetComponent<Renderer>().material.color = Color.black;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //this.tag = "EnergizedOff";
+        //GetComponent<Renderer>().material.color = Color.black;
+        if (other.gameObject.tag != "Player" && other.gameObject.tag != "ElectricalDoor")
+        {
+            this.tag = "BlackOut";
+        }
+    }
+
+    void DelayMethod()
+    {
+        //collider.size = new Vector3(1, 1, 1);
+        this.tag = "EnergizedOff";
+    }
+}
+
---
+++ Assets/Ryusei/MapChipScript/R_PowerButton2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class R_PowerButton2 : MonoBehaviour
+{
+    public bool SwitchFlg;         //スイッチが押されているかいないか
+    bool SwitchHitFlg;
+    BoxCollider collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SwitchHitFlg == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (SwitchFlg == false)
+                {
+                    this.gameObject.transform.Translate(0, -0.1f, 0);
+                    this.tag = "EnergizedOn";
+                    SwitchFlg = true;
+                }
+                else
+                {
+                    this.gameObject.transform.Translate(0, 0.1f, 0);
+                    this.tag = "EnergizedOff";
+                    SwitchFlg = false;
+                }
+            }
+        }
+    }
+
+    void DelayMethod()
+    {
+        collider.size = new Vector3(2, 2, 2);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")   //プレイヤーが触れたらSwitchHitFlgをOn
+        {
+            SwitchHitFlg = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")   //プレイヤーが離れたらSwitchHitFlgをOff
+        {
+            SwitchHitFlg = false;
+        }
+    }
+}
