d513dbca3eb4ea80208db808cbc212b3b3ce858d
libra015 <11806015@stu.kbc.ac.jp>
2020-10-29 13:21:10 +0900

    パンプキンと電気発生装置のオブジェ完成。

---
+++ Assets/Maiko/MaterialChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChange : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    bool MatChange = false;
+    [SerializeField] Material[] materials1;
+    [SerializeField] Material[] materials2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha0))
+        {
+            MatChange = true;
+            
+        }else if (Input.GetKey(KeyCode.Alpha8))
+        {
+            MatChange = false;
+        }
+        meshRenderer.materials = MatChange ? materials2 : materials1;
+    }
+}
