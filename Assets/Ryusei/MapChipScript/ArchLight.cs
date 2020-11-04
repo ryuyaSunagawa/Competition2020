be262cbfab2e15a978692da6e8edc02459cc88f7
nishishimamoto <11807020@stu.kbc.ac.jp>
2020-11-04 15:41:44 +0900

    電気を通し光るアーチ

--- Assets/Ryusei/MapChipScript/ArchLight.cs
+++ Assets/Ryusei/MapChipScript/ArchLight.cs
@@ -1,54 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class R_ElectricalWire2 : MonoBehaviour
+public class ArchLight : MonoBehaviour
 {
+    int changeColor;
+    int beforeColor;
+
+    MeshRenderer meshRenderer;//
+    bool MatChange = false;//
+    [SerializeField] Material[] materials1;//
+    [SerializeField] Material[] materials2;//
+
+    public bool hasLight;
 
     GameObject PowerButton;
-    int changeColor;    //0,黒 1,黄色
-    int beforeColor;
 
     // Start is called before the first frame update
     void Start()
     {
+        meshRenderer = GetComponent<MeshRenderer>();
+
         PowerButton = GameObject.FindGameObjectWithTag("PowerButton");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if( beforeColor != changeColor)
+        if (beforeColor != changeColor)
         {
             if (changeColor == 0)
             {
-                GetComponent<Renderer>().material.color = Color.black;
+                MatChange = false;
+                hasLight = false;
             }
             else
             {
-                GetComponent<Renderer>().material.color = Color.yellow;
+                MatChange = true;
+                hasLight = true;
             }
 
         }
         beforeColor = changeColor;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-
-        if (other.gameObject.tag != "Player" && other.gameObject.tag != "ElectricalDoor")
-        {
-            this.tag = "BlackOut";
-        }else if(other.gameObject.tag == "Player" && gameObject.tag == "EnergizedOn")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name); //シーン再読み込み(感電)
-        }
+        meshRenderer.materials = MatChange ? materials2 : materials1;//
     }
 
     private void OnTriggerStay(Collider other)
     {
-
         if (other.gameObject.tag == "BlackOut")
         {
             this.tag = "BlackOut";
@@ -56,20 +54,20 @@ public class R_ElectricalWire2 : MonoBehaviour
 
         if (gameObject.tag == "BlackOut")
         {
+            //GetComponent<Renderer>().material.color = Color.black;
             changeColor = 0;
             Invoke("DelayMethod", 0.02f);
         }
-        else if (other.gameObject.tag == "EnergizedOn" && PowerButton.tag == "EnergizedOn")
+        else if (other.gameObject.tag == "EnergizedOn" && PowerButton.tag == "EnergizedOn")  //点灯
         {
-            this.tag = "EnergizedOn";
             changeColor = 1;
+            this.tag = "EnergizedOn";
         }
-        else if (other.gameObject.tag == "EnergizedOff" && PowerButton.tag == "EnergizedOff")
+        else if (other.gameObject.tag == "EnergizedOff" && PowerButton.tag == "EnergizedOff")    //消灯
         {
-            this.tag = "EnergizedOff";
             changeColor = 0;
+            this.tag = "EnergizedOff";
         }
-
     }
 
     private void OnTriggerExit(Collider other)
@@ -85,4 +83,3 @@ public class R_ElectricalWire2 : MonoBehaviour
         this.tag = "EnergizedOff";
     }
 }
-
