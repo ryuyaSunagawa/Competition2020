b170ce344cd9a2b3a9206ffeff3545f0712aadae
ryuyaSunagawa <11806134@stu.kbc.ac.jp>
2020-11-05 12:08:56 +0900

    カメラ一応完成

--- Assets/Ryuya/Script/StageCamera.cs
+++ Assets/Ryuya/Script/StageCamera.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class StageCamera : MonoBehaviour
 {
-	//Camera myCam;
 	Transform verTransform = null;
 	Transform horTransform = null;
 	[SerializeField] Transform centerObj;
-	[SerializeField, Range( 0f, 1000f )] float speed = 10f;
-
+	[SerializeField, Range( 0, 100 )] float speed = 10f;
+	[SerializeField, Header( "中心からのカメラの距離" )] float centerDistance = 10f;
+	float beforeCD = 10f;
+	[SerializeField, Header( "シーンビュー内でカメラが中点を向くか" )] bool beTargetCenter = true;
 	Vector3 initialVec = new Vector3();
-    // Start is called before the first frame update
-    void Start()
+
+	private void Awake()
+	{
+		speed *= 10f;
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
-		initialVec = transform.position - centerObj.position;
-		//myCam = GetComponent<Camera>();
+		initialVec = ( new Vector3( 0f, 0f, -10f ) * centerDistance ) - centerObj.position;
+		//中心からの距離を計算し、代入
+		transform.position = ( transform.position - centerObj.position ).normalized * centerDistance;
     }
 
     // Update is called once per frame
@@ -24,58 +32,44 @@ public class StageCamera : MonoBehaviour
 		float xAxis = speed * Input.GetAxis( "Mouse X" ) * Time.deltaTime;
 		float yAxis = speed * Input.GetAxis( "Mouse Y" ) * Time.deltaTime;
 
+		Debug.Log( yAxis );
+
 		this.transform.LookAt( centerObj );
 		//水平方向回転
 		transform.RotateAround( centerObj.position, transform.up, xAxis );
 
-		//垂直方向回転(制御系)
-
-		transform.RotateAround( centerObj.position, transform.right, yAxis );
-
-		Debug.Log( transform.eulerAngles + ", " + transform.localEulerAngles );
-		//Debug.Log( Vector3.Cross( initialVec, transform.position - centerObj.position ) );
-		//Debug.Log( LimitRotate( yAxis ) );
+		//垂直方向回転
+		transform.RotateAround( centerObj.position, transform.right, LimitRotate( yAxis ) );
 	}
 
 	private void OnDrawGizmos()
 	{
-		Vector3 ahan1 = transform.position + transform.right;
+		if( beTargetCenter ) transform.LookAt( centerObj );
+		transform.position = ( transform.position - centerObj.position ).normalized * centerDistance;
 		Gizmos.color = Color.green;
-		Gizmos.DrawLine( transform.position, ahan1 );
-		//Debug.Log( "dist: " + Vector3.Distance( transform.position, transform.position + transform.right ) );
+		Gizmos.DrawLine( centerObj.position, transform.position.normalized * centerDistance );
 	}
 
 	/// <summary>
 	/// 回転制御関数
 	/// </summary>
-	/// <param name="rot">現在のオブジェクトの回転</param>
 	/// <param name="deltaRot">現在の回転</param>
 	/// <returns></returns>
 	private float LimitRotate( float deltaRot )
 	{
-		float whichDir = Vector3.Cross( initialVec, transform.position - centerObj.position ).x > 0 ? 1 : -1;
+		float whichDir = Vector3.Cross( initialVec, transform.position - centerObj.position ).x >= 0 ? 1 : -1;
+		float nextRot = transform.localEulerAngles.x + deltaRot;
 
-		if( whichDir == 1 && transform.localEulerAngles.x + deltaRot >= 75f )
+		if ( whichDir == 1 && nextRot >= 75f ) 
 		{
-			return deltaRot - (transform.localEulerAngles.x + deltaRot - 75f );
+			Debug.Log( deltaRot - ( nextRot - 75f ) );
+			return deltaRot - ( nextRot - 75f );
 		}
-		else if( whichDir == -1 && transform.localEulerAngles.x - deltaRot <= 285f )
+		else if ( whichDir == -1 && nextRot <= 285f ) 
 		{
-
-			return deltaRot;
+			return deltaRot + ( 285f - ( nextRot ) );
 		}
 
-		return whichDir;
-
-		//if( ( rot.x > 1 ) && ( rot.x + deltaRot ) >= 75f )
-		//{
-			
-		//	return 75f;
-		//} else if( ( rot.x < 0 ) && ( rot.x + deltaRot ) <= 265f )
-		//{
-		//	return 265f;
-		//}
-
-		//return rot.x + deltaRot;
+		return deltaRot;
 	}
 }
