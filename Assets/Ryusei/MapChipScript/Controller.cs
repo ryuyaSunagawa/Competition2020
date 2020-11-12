cdebdc52db44382f62bce6a4b49133612dc58a55
nishishimamoto <11807020@stu.kbc.ac.jp>
2020-11-12 23:10:05 +0900

    コントローラのボタン取得(途中)

--- Assets/Ryusei/MapChipScript/Controller.cs
+++ Assets/Ryusei/MapChipScript/Controller.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+
+    float x , y;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +15,18 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Left"))
-        {
-            Debug.Log("LボタンorQキー");
-        }
-        if (Input.GetButtonDown("Right"))
-        {
-            Debug.Log("RボタンorEキー");
-        }
+        if (Input.GetButtonDown("LB")) Debug.Log("LBボタンorQキー");
+        if (Input.GetButtonDown("RB")) Debug.Log("RBボタンorEキー");
+        if (Input.GetButtonDown("B")) Debug.Log("BボタンorSpaceキー");
+        if (Input.GetButtonDown("X")) Debug.Log("XボタンorSpaceキー");
+        if (Input.GetButtonDown("A")) Debug.Log("AボタンorSpaceキー"); 
+        if (Input.GetAxis("Horizontal") < 0) Debug.Log("左スティック←");
+        if (Input.GetAxis("Horizontal") > 0) Debug.Log("左スティック→");
+        if (Input.GetAxis("Vertical") > 0) Debug.Log("左スティック↑");
+        if (Input.GetAxis("Vertical") < 0) Debug.Log("左スティック↓");
+        if (Input.GetAxis("R_Horizontal") < 0) Debug.Log(Input.GetAxis("R_Horizontal"));
+        if (Input.GetAxis("R_Horizontal") > 0) Debug.Log(Input.GetAxis("R_Horizontal"));
+        if (Input.GetAxis("R_Vertical") > 0) Debug.Log(Input.GetAxis("R_Vertical"));
+        if (Input.GetAxis("R_Vertical") < 0) Debug.Log(Input.GetAxis("R_Vertical"));
     }
 }
