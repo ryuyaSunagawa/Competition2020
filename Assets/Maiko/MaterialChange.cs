f1a48d62d0de3404e8d076bc145c7cb4c41281f6
ryuyaSunagawa <11806134@stu.kbc.ac.jp>
2020-12-03 13:09:08 +0900

    かぼちゃの発光仕様を変更

--- Assets/Maiko/MaterialChange.cs
+++ Assets/Maiko/MaterialChange.cs
@@ -9,23 +9,53 @@ public class MaterialChange : MonoBehaviour
     [SerializeField] Material[] materials1;
     [SerializeField] Material[] materials2;
 
+	[SerializeField] MiniLight lightObj = null;
+	[SerializeField] bool isPumpkinObject = false;
+
+	int pumpkinChange = 0;
+	int beforePumpkin = 0;
+
+	[SerializeField] Transform[] panelTransform = new Transform[ 3 ];
+
     // Start is called before the first frame update
     void Start()
     {
         meshRenderer = GetComponent<MeshRenderer>();
+			ChangePanelActive( false );
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Alpha0))
-        {
-            MatChange = true;
-            
-        }else if (Input.GetKey(KeyCode.Alpha8))
-        {
-            MatChange = false;
-        }
+		if ( isPumpkinObject )
+		{
+			pumpkinChange = lightObj.changeColor;
+
+			if ( pumpkinChange != beforePumpkin )
+			{
+				if ( lightObj.changeColor == 1 )
+				{
+					MatChange = true;
+					ChangePanelActive( true );
+				}
+				else if ( lightObj.changeColor == 0 )
+				{
+					MatChange = false;
+					ChangePanelActive( false );
+				}
+			}
+
+			beforePumpkin = pumpkinChange;
+		}
+
         meshRenderer.materials = MatChange ? materials2 : materials1;
     }
+
+	void ChangePanelActive( bool flag )
+	{
+		foreach ( Transform changeActive in panelTransform )
+		{
+			changeActive.gameObject.SetActive( flag );
+		}
+	}
 }
